5722139c8c50469c0623d1cbf322887079176900
adith442864 <adith442864@gmail.com>
2021-11-25 12:30:44 +0530

    cherry third commit

--- src/Pages/CherryPage.java
+++ src/Pages/CherryPage.java
@@ -1,6 +1,10 @@
 package Pages;
 
 public class CherryPage {
+	
+	public void test() {
+		System.out.println("test feature...");
+	}
 
 	public static void main(String[] args) {
 		
@@ -8,8 +12,10 @@ public class CherryPage {
 		int sal=100;
 		
 		
+		
+		
 		// a9ee4bb (HEAD -> master) first cherry commit
-		//
+		// 1cebe53 (HEAD -> master) second cherry commit
 		//
 		//
 		//
