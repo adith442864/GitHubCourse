1cebe5333a91e7fd64ea9ecbb72e67f21d017210
adith442864 <adith442864@gmail.com>
2021-11-25 12:29:37 +0530

    second cherry commit

--- src/Pages/CherryPage.java
+++ src/Pages/CherryPage.java
@@ -5,6 +5,15 @@ public class CherryPage {
 	public static void main(String[] args) {
 		
 		String name="Adith";
+		int sal=100;
+		
+		
+		// a9ee4bb (HEAD -> master) first cherry commit
+		//
+		//
+		//
+		//
+		
 
 	}
 
