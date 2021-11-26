79aa8dc580b1087e10047c8f22a1909e762228c7
adith442864 <adith442864@gmail.com>
2021-11-26 15:29:35 +0530

    added contacts page files

--- src/Pages/ContactsPage.java
+++ src/Pages/ContactsPage.java
@@ -4,8 +4,13 @@ public class ContactsPage {
 	
 	public void contacts() {
 		
+<<<<<<< Updated upstream
 		String name2="contact d2";
 		System.out.println(name2);
+=======
+		String name1="contact d1";
+		System.out.println(name1);
+>>>>>>> Stashed changes
 	}
 
 }
