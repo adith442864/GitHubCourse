473ea8b0715009bfdacb0af012c3ec0e29d1661e
adith442864 <adith442864@gmail.com>
2021-11-30 13:08:57 +0530

    cart page added

---
+++ src/Pages/CartPage.java
@@ -0,0 +1,22 @@
+package Pages;
+
+public class CartPage {
+	
+	public void cart() {
+		
+		String nameD1 = "cart D1";
+		System.out.println(nameD1);
+		
+		if(true) {
+			System.out.println("cart is done");
+		}
+		
+		while(true) {
+			System.out.println("done....");
+			break;
+		}
+	}
+	
+	
+
+}
